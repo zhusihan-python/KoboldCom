6b2623686135b3bb1837a0668e20907e8eced4b9
allenz <1009698013@qq.com>
2025-05-12 15:38:12 +0800

    update net 9

--- KoboldCom/Properties/AssemblyInfo.cs
+++ KoboldCom/Properties/AssemblyInfo.cs
@@ -1,15 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-// 有关程序集的常规信息通过以下
-// 特性集控制。更改这些特性值可修改
-// 与程序集关联的信息。
-[assembly: AssemblyTitle("KoboldCom")]
-[assembly: AssemblyDescription("Serial Port Communication Tool")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KoboldCom")]
-[assembly: AssemblyCopyright("Copyright © Shizhy  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,16 +10,3 @@ using System.Runtime.InteropServices;
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
 [assembly: Guid("02a1fd7b-dff1-4f86-a5ea-b3039eda78e0")]
-
-// 程序集的版本信息由下面四个值组成:
-//
-//      主版本
-//      次版本 
-//      内部版本号
-//      修订号
-//
-// 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
-// 方法是按如下所示使用“*”:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
